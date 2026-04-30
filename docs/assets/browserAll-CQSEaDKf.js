57d047145645c1e8bd9fdb18773ec3729ad96c3b
alenaksu <alenaksu@users.noreply.github.com>
2026-04-30 08:18:17 +0000

    chore: build pages

---
+++ docs/assets/browserAll-CQSEaDKf.js
@@ -0,0 +1,2 @@
+import{e as a,A as e,C as i,a as s,D as t,E as n,F as d}from"./pixi-BwpucdPl.js";import"./webworkerAll-CNrv0g9Y.js";a.add(e);a.mixin(i,s);a.add(t);a.add(n);a.mixin(i,d);
+//# sourceMappingURL=browserAll-CQSEaDKf.js.map
---
+++ docs/assets/webworkerAll-CNrv0g9Y.js
@@ -0,0 +1,2 @@
+import{e,c as s,d as a,f as t}from"./pixi-BwpucdPl.js";e.add(s,a);e.add(t);
+//# sourceMappingURL=webworkerAll-CNrv0g9Y.js.map
